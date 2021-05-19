4e3af2d8f8cc5986d7e1b5f28bbcc808e87783d1
Jean-Sébastien Goupil <contact@jsgoupil.com>
2021-05-19 15:40:12 -0700

    Initial commit.

---
+++ example/BarcodePDF417.Core.Console/ExamplePDF417.cs
@@ -0,0 +1,66 @@
+﻿using BarcodeBakery.Barcode;
+using BarcodeBakery.Common;
+using System;
+using System.Threading.Tasks;
+
+namespace BarcodePDF417.Core.Console
+{
+    public class ExamplePDF417
+    {
+        public static async Task CreateAsync(string filePath, string? text = null)
+        {
+            // Loading Font
+            var font = new BCGFont("Arial", 18);
+
+            // Don't forget to sanitize user inputs
+            text = text?.Length > 0 ? text : "PDF417";
+
+            // Label, this part is optional
+            var label = new BCGLabel();
+            label.SetFont(font);
+            label.SetPosition(BCGLabel.Position.Bottom);
+            label.SetAlignment(BCGLabel.Alignment.Center);
+            label.SetText(text);
+
+            // The arguments are R, G, B for color.
+            var colorBlack = new BCGColor(0, 0, 0);
+            var colorWhite = new BCGColor(255, 255, 255);
+
+            Exception? drawException = null;
+            BCGBarcode? barcode = null;
+            try
+            {
+                var code = new BCGpdf417();
+                code.SetScale(2);
+                code.SetRatio(1);
+                code.SetErrorLevel(2);
+                code.SetCompact(false);
+                code.SetQuietZone(true);
+                code.SetForegroundColor(colorBlack); // Color of bars
+                code.SetBackgroundColor(colorWhite); // Color of spaces
+
+                code.AddLabel(label);
+
+                code.Parse(text);
+                barcode = code;
+            }
+            catch (Exception exception)
+            {
+                drawException = exception;
+            }
+
+            var drawing = new BCGDrawing(barcode, colorWhite);
+            if (drawException != null)
+            {
+                drawing.DrawException(drawException);
+            }
+
+            // Saves the barcode into a file.
+            await drawing.FinishAsync(BCGDrawing.ImageFormat.Png, filePath);
+
+            // Saves the barcode into a MemoryStream
+            ////var memoryStream = new System.IO.MemoryStream();
+            ////await drawing.FinishAsync(BCGDrawing.ImageFormat.Png, memoryStream);
+        }
+    }
+}
---
+++ example/BarcodePDF417.Core.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace BarcodePDF417.Core.Console
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // See each barcode file to see how you can save to a file or a MemoryStream.
+            await ExamplePDF417.CreateAsync("barcode_pdf417.png");
+        }
+    }
+}
